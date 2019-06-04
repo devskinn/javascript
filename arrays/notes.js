5e6b26aeb055ba95a8a6ac348aea3be7736f4114
devskinn <pskin876@gmail.com>
2019-06-04 21:28:01 +0100

    changed files

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,23 +1,23 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+/*const notes = ['Note 1', 'Note 2', 'Note 3']
 console.log(notes)
 // length property - get number of items in the array
-// console.log(notes.length)
+console.log(notes.length)
 // getting array values
-// console.log(notes[1]) // index starts at zero
-// console.log(notes[6]) // element does not exist so undefined returned
-// console.log(notes[notes.length - 1]) // get last item in the array
+console.log(notes[1]) // index starts at zero
+console.log(notes[6]) // element does not exist so undefined returned
+console.log(notes[notes.length - 1]) // get last item in the array
 
 // Push and Pop methods - works with items at end of the array
-// notes.pop() // removes last item
-// console.log(notes)
-// notes.push('Note 4') // adds item to end of array
-// console.log(notes)
+notes.pop() // removes last item
+console.log(notes)
+notes.push('Note 4') // adds item to end of array
+console.log(notes)
 
 // Shift and Unshift methods - works with items at start of the array
-// notes.shift() // removes first item
-// console.log(notes)
-// notes.unshift('Note 0') // adds item at start of array
-// console.log(notes)
+notes.shift() // removes first item
+console.log(notes)
+notes.unshift('Note 0') // adds item at start of array
+console.log(notes)
 
 // Splice method - good for adding and removing items
 notes.splice(1, 1) // starts at 2nd item and removes one item
@@ -28,3 +28,40 @@ notes.splice(1, 1, 'Newer Note 2') // replaces 2nd item in the array
 console.log(notes)
 notes[2] = 'New Note 3' // best method to use when replacing items
 console.log(notes)
+
+// Callback Function: One function calling another function. e.g. to loop through notes array
+// can use 2 params - index and item
+notes.forEach(function (item, index) {
+    console.log(index + ': ' + item)
+})
+
+// alternate to above is to use a for loop (gives more flexibility)
+for (let count = 0; count < notes.length; count++) {
+    console.log(notes[count])
+}
+// same in reverse
+for (let count = notes.length - 1; count >= 0; count--) {
+    console.log(notes[count])
+}
+*/
+
+// an array of objects - still works same, pop, push etc
+const notes = [{
+    title: 'My road trip',
+    body: 'brmm brmm brmmmmm'
+}, {
+    title: 'My dogs bark',
+    body: 'woof woof ruff'
+}, {
+    title: 'My wifes voice',
+    body: 'nag nag nag'
+}]
+
+const findNote = function (notes, noteTitle) {
+    const index = notes.findIndex(function (note, index) {
+        return note.title === noteTitle
+    })
+    return notes[index]
+}
+const note = findNote(notes, 'My dogs bark')
+console.log(note)
--- arrays/todo.js
+++ arrays/todo.js
@@ -2,7 +2,7 @@
 // add new item to end
 // remove first item
 const todos = ['walk dog', 'go shopping', 'phone dentist', 'email boss', 'book flights']
-
+/*
 console.log(todos)
 todos.splice(2, 1)
 console.log(todos)
@@ -11,8 +11,15 @@ console.log(todos)
 todos.shift()
 console.log(todos)
 
+console.log(`You have ${todos.length} ToDo items`)
+todos.forEach(function (item, index) {
+    console.log(`${index + 1}. ${item}`)
+})
+*/
 
-//console.log(`You have ${todos.length} ToDo items`)
-//console.log(todos)
+// same as above using for loop
+for (count = 0; count < todos.length; count++) {
+    console.log(`${count + 1}. ${todos[count]}`)
+}
 
 
